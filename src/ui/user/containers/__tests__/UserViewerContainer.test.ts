48323da6a53c2ffddd15dff40bb208a0b517291c
César Alberca <cesalberca@gmail.com>
2019-06-01 13:01:17 +0200

    Update deps

--- src/ui/user/containers/__tests__/UserViewerContainer.test.ts
+++ src/ui/user/containers/__tests__/UserViewerContainer.test.ts
@@ -19,10 +19,9 @@ describe("UserViewerContainer", () => {
     avatarQueryEmailServiceMock = {
       queryEmail: jest.fn().mockResolvedValue(new User("foo", "bar"))
     };
+
     avatarRepositoryMock = {
-      getUserByEmailHash: jest
-        .fn()
-        .mockReturnValue(Promise.resolve(new User("foo", "foo")))
+      getUserByEmailHash: jest.fn().mockResolvedValue(new User("foo", "foo"))
     };
 
     wrapper = shallowMount(UserViewerContainer, {
@@ -33,12 +32,10 @@ describe("UserViewerContainer", () => {
     });
   });
 
-  it("should call service when email changes", async () => {
-    expect.assertions(1);
+  it("should call service when email changes", () => {
     const userFormComponent = wrapper.find({ name: "UserFormContainer" });
 
     userFormComponent.vm.$emit("on-valid-email-change", "foo@foo.com");
-    await flushPromises();
 
     expect(avatarQueryEmailServiceMock.queryEmail).toHaveBeenCalled();
   });
