5e31a6ccc6a040eba5c3839975f24779a8fbda65
César Alberca <cesalberca@gmail.com>
2019-06-21 12:53:24 +0200

    4.3. Make 5th test pass

--- src/katas/FileCreator.ts
+++ src/katas/FileCreator.ts
@@ -1,14 +1,22 @@
 export class FileCreator {
+  private originalName = "";
+
   public constructor(private readonly files: string[]) {}
 
   public overwrite(name: string) {
     return name;
   }
 
-  public copy(name: string) {
-    if (this.files.includes(name)) {
-      return `${name} (1)`;
+  public copy(name: string, n = 1): string {
+    if (n === 1) {
+      this.originalName = name;
     }
-    return name;
+
+    if (!this.files.includes(name)) {
+      return name;
+    }
+
+    const nextName = `${this.originalName} (${n})`;
+    return this.copy(nextName, n + 1);
   }
 }
