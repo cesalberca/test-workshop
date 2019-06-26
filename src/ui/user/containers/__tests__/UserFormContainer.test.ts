c474e4950a7e6625101c3529b68a713e73c9eb0d
César Alberca <cesalberca@gmail.com>
2019-06-26 10:10:37 +0200

    Add missing tests

--- src/ui/user/containers/__tests__/UserFormContainer.test.ts
+++ src/ui/user/containers/__tests__/UserFormContainer.test.ts
@@ -1,7 +1,41 @@
-import UserFormContainer from '../UserFormContainer.vue'
+import UserFormContainer from "../UserFormContainer.vue";
+import { shallowMount } from "@vue/test-utils";
 
-describe('{ UserFormContainer }', () => {
-  it('', () => {
+describe("UserFormContainer", () => {
+  it("should emit email after it is validated correctly", () => {
+    const { wrapper, userValidationService } = createWrapper();
+    userValidationService.validate.mockReturnValue(true);
 
-  })
-})
+    const component = wrapper.find({ name: "UserFormComponent" });
+    component.vm.$emit("on-email-change", "foo@bar.com");
+    const actual = wrapper.emitted("on-valid-email-change");
+
+    expect(actual).toEqual([["foo@bar.com"]]);
+  });
+
+  it("should not emit email after it is validated incorrectly", () => {
+    const { wrapper, userValidationService } = createWrapper();
+    userValidationService.validate.mockReturnValue(false);
+
+    const component = wrapper.find({ name: "UserFormComponent" });
+    component.vm.$emit("on-email-change", "foo@bar.com");
+    const actual = wrapper.emitted("on-valid-email-change");
+
+    expect(actual).not.toEqual([["foo@bar.com"]]);
+  });
+});
+
+function createWrapper(props: object = {}) {
+  const userValidationService = { validate: jest.fn().mockReturnValue(true) };
+  return {
+    wrapper: shallowMount(UserFormContainer, {
+      propsData: {
+        ...props
+      },
+      provide: {
+        userValidationService
+      }
+    }),
+    userValidationService
+  };
+}
